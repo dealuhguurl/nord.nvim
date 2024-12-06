8e2137209b8b3c9fe272fe655b7917280ab2b6ee
dealuhguurl <157326414+dealuhguurl@users.noreply.github.com>
2024-12-05 17:53:53 -0800

    changed color of telescope title

--- lua/nord/plugins/picker.lua
+++ lua/nord/plugins/picker.lua
@@ -6,7 +6,9 @@ local utils = require("nord.utils")
 function picker.highlights()
   return {
     TelescopeBorder = { fg = c.polar_night.light, bg = utils.make_global_bg(true) },
-    TelescopeTitle = { fg = c.snow_storm.origin, bold = true },
+    --TelescopeTitle = { fg = c.snow_storm.origin, bold = true },
+    TelescopeTitle = { fg = c.polar_night.origin, bg = c.aurora.purple },
+    TelescopePromptPrefix = { fg = c.frost.ice },
     TelescopePromptCounter = { fg = c.polar_night.light },
     TelescopeMatching = { fg = c.frost.ice, bold = true },
   }
